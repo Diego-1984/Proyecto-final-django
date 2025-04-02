1df3588f254cbd3dd207873693959233035fa12b
Diego <116676429+Diego-1984@users.noreply.github.com>
2025-04-02 13:19:05 +0200

    Funcionalidad agregar al carro finalizada

--- Proyecto-web-django/Carroapp/views.py
+++ Proyecto-web-django/Carroapp/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render, redirect
 from .carro import Carro
 from Tiendapp.models import Producto
+from django.contrib import messages
 
 # Create your views here.
 
 def agregar_producto(request, producto_id):
     carro = Carro(request)
-    producto = Producto.objects.get(id = producto_id)
+    producto = Producto.objects.get(id=producto_id)
     carro.agregar(producto=producto)
-    return redirect("tienda")
+    
+    messages.success(request, f'🛒 {producto.nombre} agregado al carrito')
+
+    return redirect(request.META.get('HTTP_REFERER', 'Tienda'))
 
 def eliminar_producto(request, producto_id):
     carro = Carro(request)
     producto = Producto.objects.get(id = producto_id)
     carro.eliminar(producto=producto)
-    return redirect("tienda")
+    return redirect("carro:Carro")
 
 def restar_producto(request, producto_id):
     carro = Carro(request)
     producto = Producto.objects.get(id = producto_id)
     carro.restar_producto(producto=producto)
-    return redirect("tienda")
+    return redirect("carro:Carro")
 
 def limpiar_carro(request, producto_id):
     carro = Carro(request)
     carro.limpiar_carro()
-    return redirect("tienda")
+    return redirect("carro:Carro")
 
 def carro(request):
     return render(request, "carro\carro.html")
