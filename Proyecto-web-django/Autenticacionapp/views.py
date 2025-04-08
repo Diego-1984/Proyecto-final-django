82db675777813933fcac4eb52ed7378fa2392f15
Diego <116676429+Diego-1984@users.noreply.github.com>
2025-04-08 19:02:28 +0200

    Proceso de registro finalizado y funcionando

--- Proyecto-web-django/Autenticacionapp/views.py
+++ Proyecto-web-django/Autenticacionapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib import messages
 
 # Create your views here.
@@ -20,3 +20,7 @@ class VRegistro(View):
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
             return render(request, "registro/registro.html", {"form":form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('Home')
