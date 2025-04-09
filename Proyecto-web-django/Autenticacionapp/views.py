f2adf019e4f3a6cc83fcc163ecd81e2ea90ef9db
Diego <116676429+Diego-1984@users.noreply.github.com>
2025-04-09 20:03:13 +0200

    Creado formulario login y funcionalidad de logearse

--- Proyecto-web-django/Autenticacionapp/views.py
+++ Proyecto-web-django/Autenticacionapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 # Create your views here.
@@ -24,3 +24,21 @@ class VRegistro(View):
 def cerrar_sesion(request):
     logout(request)
     return redirect('Home')
+
+def logearse(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            usuario = authenticate(username = nombre_usuario, password = contraseña)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect("Home")
+            else:
+                messages.error(request,"Usuario no válido")
+        else:
+            messages.error(request,"Información incorrecta")
+
+    form = AuthenticationForm()
+    return render(request, "login/login.html", {"form":form})
